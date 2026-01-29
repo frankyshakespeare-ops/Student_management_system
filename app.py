06c69ae11b2db0b745c93ca94353a345b9c5c2b5
Francky_shakespeare <frankyshakespeare@gmail.com>
2026-01-30 00:41:44 +0300

    CRUD Classes

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 import sqlite3
 
 app = Flask(__name__)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "database.db")
+
 def get_db_connection():
-    conn = sqlite3.connect("database.db")
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
+
 
 @app.route("/")
 def home():
     return redirect(url_for("students"))
+
 
 # fetch all students
 @app.route("/students")
@@ -19,6 +25,7 @@ def students():
     students = conn.execute("SELECT * FROM students").fetchall()
     conn.close()
     return render_template("students.html", students=students)
+
 
 # add one student
 @app.route("/students/add", methods=["GET", "POST"])
@@ -41,5 +48,34 @@ def add_student():
     return render_template("add_student.html")
 
 
+# fetch all classes
+@app.route("/classes")
+def classes():
+    conn = get_db_connection()
+    classes = conn.execute("SELECT * FROM classes").fetchall()
+    conn.close()
+    return render_template("classes.html", classes=classes)
+
+
+# add one class
+@app.route("/classes/add", methods=["GET", "POST"])
+def add_class():
+    if request.method == "POST":
+        name = request.form["name"]
+        level = request.form["level"]
+
+        conn = get_db_connection()
+        conn.execute(
+            "INSERT INTO classes (name, level) VALUES (?, ?)",
+            (name, level)
+        )
+        conn.commit()
+        conn.close()
+        return redirect(url_for("classes"))
+
+    return render_template("add_class.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
