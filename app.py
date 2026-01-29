92ff8ff8dcd013e0d8f7e36038f69a13811668f3
Francky_shakespeare <frankyshakespeare@gmail.com>
2026-01-29 23:05:36 +0300

    app.py (Student management version 1)

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route("/")
 def home():
-    return "Hello Student Management System"
+    return redirect(url_for("students"))
+
+# fetch all students
+@app.route("/students")
+def students():
+    conn = get_db_connection()
+    students = conn.execute("SELECT * FROM students").fetchall()
+    conn.close()
+    return render_template("students.html", students=students)
+
+# add one student
+@app.route("/students/add", methods=["GET", "POST"])
+def add_student():
+    if request.method == "POST":
+        name = request.form["name"]
+        matricule = request.form["matricule"]
+        dob = request.form["date_of_birth"]
+        gender = request.form["gender"]
+
+        conn = get_db_connection()
+        conn.execute(
+            "INSERT INTO students (name, matricule, date_of_birth, gender) VALUES (?, ?, ?, ?)",
+            (name, matricule, dob, gender),
+        )
+        conn.commit()
+        conn.close()
+        return redirect(url_for("students"))
+
+    return render_template("add_student.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
